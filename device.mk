a82ce73c0f15f6ec4950a64da88544fe91ceb726
Wang Han <416810799@qq.com>
2020-06-12 14:08:26 +0800

    oneplus2: Rename init.recovery.oneplus2.rc to init.recovery.qcom.rc
    Change-Id: Iff1a6061890c85d8fc49282d5593a297e785f501

--- device.mk
+++ device.mk
@@ -249,7 +249,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
-    init.recovery.oneplus2.rc \
+    init.recovery.qcom.rc \
     ueventd.qcom.rc
 
 # Recovery
